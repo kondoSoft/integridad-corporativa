652d83d5748c4d6ac8994d2849e2617e815ddf99
Luis Juarez <skaflon_war@hotmail.com>
2017-10-28 11:41:20 -0500

    change data in glosario page

--- src/data.js
+++ src/data.js
@@ -16,52 +16,78 @@ export const data = [
                   mínimos de integridad y acciones disciplinarias para la organización y su personal (Transparencia Internacional, 2009, pág. 8).`
   },
   {
-    title: 'Código de ética o de conducta',
-    description: `Declaración de principios y valores que establece un conjunto de expectativas y estándares de cómo se comportará una
-                  organización, cuerpo gubernamental, compañía, grupo afiliado, directivo, empleado o socio, teniendo en cuenta niveles
-                  mínimos de integridad y acciones disciplinarias para la organización y su personal (Transparencia Internacional, 2009, pág. 8).`
+    title: 'Confidencialidad o anonimidad del denunciante',
+    description: `Imposibilidad de que el denunciado rastree el origen de la denuncia.
+    Las empresas y organizaciones deben establecer los procedimientos y medidas para facilitar e incentivar la denuncia para
+    que, en caso de experimentar o presenciar sobornos u otros actos de corrupción, cuenten con los medios necesarios para
+    denunciar a través de mecanismos que garanticen la confidencialidad y anonimidad del denunciante.
+    Adicionalmente, es deseable que la empresa u organización establezca un mecanismo para el acompañamiento del
+    denunciante ante la autoridad si así procede, sobre todo cuando sea necesario solicitar medidas de protección.`
   },
   {
-    title: 'Código de ética o de conducta',
-    description: `Declaración de principios y valores que establece un conjunto de expectativas y estándares de cómo se comportará una
-                  organización, cuerpo gubernamental, compañía, grupo afiliado, directivo, empleado o socio, teniendo en cuenta niveles
-                  mínimos de integridad y acciones disciplinarias para la organización y su personal (Transparencia Internacional, 2009, pág. 8).`
+    title: 'Fraude',
+    description: `Delito que comete quien, engañando a uno o aprovechándose del error de alguien, se hace ilícitamente de algún beneficio
+                  o alcanza un lucro indebido (Código Penal Federal, art. 386).
+                  Los gobiernos, las empresas y las organizaciones de la sociedad civil deben contar con canales eficaces de notificación
+                  interna y mecanismos de seguimiento para detectar el fraude, la corrupción y la mala gestión en el seno de una
+                  organización.`
   },
   {
-    title: 'Código de ética o de conducta',
-    description: `Declaración de principios y valores que establece un conjunto de expectativas y estándares de cómo se comportará una
-                  organización, cuerpo gubernamental, compañía, grupo afiliado, directivo, empleado o socio, teniendo en cuenta niveles
-                  mínimos de integridad y acciones disciplinarias para la organización y su personal (Transparencia Internacional, 2009, pág. 8).`
+    title: 'Lavado de dinero',
+    description: `Proceso de ocultar el origen, la propiedad o el destino del dinero obtenido de manera ilegal o deshonesta, ocultándolo en actividades económicas legítimas para hacerlo parecer legal. Las políticas específicas para prevenirlo deben establecer acciones o requisitos necesarios para asegurarse, en la medida de lo posible, que los recursos recibidos de algún tercero no son de procedencia ilícita. Estas acciones ayudan a proteger a la empresa en caso de necesitar explicar alguna transacción ante las autoridades (Transparencia Internacional, 2009, pág. 26).`
   },
   {
-    title: 'Código de ética o de conducta',
-    description: `Declaración de principios y valores que establece un conjunto de expectativas y estándares de cómo se comportará una
-                  organización, cuerpo gubernamental, compañía, grupo afiliado, directivo, empleado o socio, teniendo en cuenta niveles
-                  mínimos de integridad y acciones disciplinarias para la organización y su personal (Transparencia Internacional, 2009, pág. 8).`
+    title: 'Monitoreo de la política anticorrupción',
+    description: `Mecanismos que permiten identificar las fortalezas y debilidades de una política anticorrupción o de integridad, de manera que pueda mejorar y que forme parte de la gestión de una organización. Dependiendo del tamaño y recursos de la empresa, estas funciones de monitoreo recaen en un oficial de cumplimiento o algún órgano de gobierno, como un comité de ética o de auditoría.`
   },
   {
-    title: 'Código de ética o de conducta',
-    description: `Declaración de principios y valores que establece un conjunto de expectativas y estándares de cómo se comportará una
-                  organización, cuerpo gubernamental, compañía, grupo afiliado, directivo, empleado o socio, teniendo en cuenta niveles
-                  mínimos de integridad y acciones disciplinarias para la organización y su personal (Transparencia Internacional, 2009, pág. 8).`
+    title: 'Pagos facilitadores',
+    description: `Son montos relativamente pequeños pagados a funcionarios o proveedores de servicios que garantizan, facilitan o agilizan la ejecución de una acción gubernamental rutinaria a la que el ordenante tiene derecho (Transparencia Internacional, 2008, pág. 9), y constituyen una forma de soborno.
+Estas prohibiciones deben contemplarse en el código de conducta o de ética de la empresa o, en su defecto, en su política anticorrupción.`
   },
   {
-    title: 'Código de ética o de conducta',
-    description: `Declaración de principios y valores que establece un conjunto de expectativas y estándares de cómo se comportará una
-                  organización, cuerpo gubernamental, compañía, grupo afiliado, directivo, empleado o socio, teniendo en cuenta niveles
-                  mínimos de integridad y acciones disciplinarias para la organización y su personal (Transparencia Internacional, 2009, pág. 8).`
+    title: 'Política anticorrupción',
+    description: `Manifiesta el compromiso anticorrupción/cero tolerancia a la corrupción de la empresa, así como directrices específicas para prevenir el soborno, pagos facilitadores, fraudes, lavado de dinero, y conflicto de intereses, entre otros. También incorpora políticas de hospitalidad y viáticos, regalos y agradecimientos, donaciones y contribuciones políticas, y habilita mecanismos de denuncia que faciliten el reporte anónimo de cualquier comportamiento indebido o ilegal ante la propia empresa. 
+                  Esta política puede estar o no incluida en el código de ética o conducta.`
   },
   {
-    title: 'Código de ética o de conducta',
-    description: `Declaración de principios y valores que establece un conjunto de expectativas y estándares de cómo se comportará una
-                  organización, cuerpo gubernamental, compañía, grupo afiliado, directivo, empleado o socio, teniendo en cuenta niveles
-                  mínimos de integridad y acciones disciplinarias para la organización y su personal (Transparencia Internacional, 2009, pág. 8).`
+    title: 'Política de donaciones y contribuciones políticas',
+    description: `Define qué tipo de donaciones o contribuciones están permitidas, ya sea en efectivo o en especie, para apoyar una causa política. Incluye donaciones y contribuciones tales como regalos de propiedades o servicios, publicidad o actividades promocionales que respalden a un partido o personaje político, o la compra de boletos para eventos de recaudación de fondos (Transparencia Internacional, 2009, pág. 34). 
+La Ley General de Instituciones y Procedimientos Electorales (art. 41) prohíbe explícitamente que las empresas realicen aportaciones a partidos políticos o sus candidatos.`
   },
   {
-    title: 'Código de ética o de conducta',
-    description: `Declaración de principios y valores que establece un conjunto de expectativas y estándares de cómo se comportará una
-                  organización, cuerpo gubernamental, compañía, grupo afiliado, directivo, empleado o socio, teniendo en cuenta niveles
-                  mínimos de integridad y acciones disciplinarias para la organización y su personal (Transparencia Internacional, 2009, pág. 8).`
+    title: 'Política de donaciones y/o contribuciones sociales',
+    description: `Define en qué casos puede realizarse una donación o contribución en efectivo o en especie, para apoyar una causa social, así como asegurar el destino de la contribución y que no se utilice para encubrir un soborno (Transparencia Internacional, 2008, pág. 9).
+`
+  },
+  {
+    title: 'Política de hospitalidad y viáticos',
+    description: `Define qué atenciones y/o invitaciones pueden darse o aceptarse en eventos y actividades vinculadas al trabajo, así como los gastos de viaje. Incluye lineamientos sobre la cantidad de invitados, el tipo de viaje, el nivel del alojamiento y la duración de la estancia permitida. Es importante diferenciar una atención o invitación razonable a una suntuosa o excesiva que pueda dar lugar a sobornos o conflictos de intereses, aparentes o reales (Transparencia Internacional, 2008, pág. 21).`
+  },
+  {
+    title: 'Política de regalos y agradecimientos',
+    description: `Define los casos en los que pueden darse o aceptarse regalos de o a terceros, ya sean clientes, proveedores, socios comerciales o accionistas, así como los registros que deben hacerse de éstos con el fin de proteger a la empresa y a sus empleados. Es importante que los socios conozcan dicha política a fin de evitar sanciones para la empresa (Transparencia Internacional, 2008, pág. 21).`
+  },
+  {
+    title: 'Programa de capacitación ',
+    description: `Proceso de socialización de la política anticorrupción, del código de conducta o de cualquier otra medida de integridad, que incorpora la discusión y convencimiento de su contenido por parte todos los empleados, directivos, proveedores, contratistas y demás socios comerciales dentro de la organización, así como por quienes se incorporen al negocio en el futuro. (Transparencia Internacional, 2008, pág. 10).`
+  },
+  {
+    title: 'Represalias a los denunciantes',
+    description: `Acciones de reprimenda de índole institucional o personal, que pueden ser tomadas en contra de las personas que muestren una preocupación de legal o ética, informen una conducta contraria a los valores de la empresa, o que participen en una investigación. 
+Dentro de las políticas y procedimientos de denuncia se deben prohibir expresamente, e incluso deben destinar recursos para la protección de los denunciantes (Transparencia Internacional, 2008, pág. 11).`
+  },
+  {
+    title: 'Sistema de denuncia',
+    description: `Mecanismo por el cual los empleados, directivos, socios y otros terceros relacionados pueden: (i) denunciar cualquier violación al código de conducta o ética de la empresa, (ii) denunciar violaciones a la política anticorrupción o de integridad, o (iii) solicitar ayuda y realizar consultas respecto de la política anticorrupción o al código de conducta o ética de la empresa.`
+  },
+  {
+    title: 'Soborno',
+    description: `Promesa, oferta o entrega de cualquier beneficio indebido a uno o varios particulares o servidores públicos, directamente o a través de terceros, a cambio de que realicen o se abstengan de realizar un acto relacionado con sus funciones o con las de otro particular o servidor público, o bien, abusen de su influencia real o supuesta, con el propósito de obtener o mantener, para sí mismo o para un tercero, un beneficio o ventaja, con independencia de la aceptación o recepción del beneficio o del resultado obtenido (Ley General de Responsabilidades Administrativas, art. 66 y Código Penal Federal, art. 11bis, 218, 222 y 222bis, entre otros).`
+  },
+  {
+    title: 'Socios comerciales',
+    description: `Terceros que no guardan estrictamente una relación laboral con la empresa pero que directa o indirectamente prestan sus servicios para que la empresa pueda realizar sus objetivos.`
   }
 ]
 export const dataSources = [
