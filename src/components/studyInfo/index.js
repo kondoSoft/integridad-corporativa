ef72584bddf6d8def8e122bb8f9772aab1ee61a2
Luis Juarez <skaflon_war@hotmail.com>
2017-11-02 17:45:31 -0600

    methodology responsive

--- src/components/studyInfo/index.js
+++ src/components/studyInfo/index.js
@@ -19,6 +19,14 @@ const Image = styled.img`
 const ImageContainer = styled.figure`
   max-width: 100%;
   max-height: 100%;
+  ${props => {
+    if (props.Tablet) {
+      return `@media screen and (max-width: 968px) {
+        width: 75%;
+        height: 70%;
+      }`
+    }
+  }}
 `
 
 const Col = styled.div`
@@ -34,6 +42,14 @@ const Paragraph = styled.p`
   font-size: 1.2em;
   margin: 20px;
   font-weight: lighter;
+  ${props => {
+    if (props.Phone) {
+      return `@media screen and (max-width: 500px) {
+        font-size: 1em;
+        line-height: 20px;
+      }`
+    }
+  }}
 `
 
 const List = styled.ul`
@@ -45,6 +61,14 @@ const ListItem = styled.li`
   display: block;
   margin: 10px;
   line-height: 25px;
+  ${props => {
+    if (props.Phone) {
+      return `@media screen and (max-width: 500px) {
+        margin: 5px;
+        line-height: 20px;
+      }`
+    }
+  }}
 `
 
 const Icon = styled.i`
@@ -55,30 +79,37 @@ const Span = styled.span`
   color: #ED462F;
   font-weight: bold;
   font-size: 1.2em;
+  ${props => {
+    if (props.Phone) {
+      return `@media screen and (max-width: 500px) {
+        font-size: 1em;
+      }`
+    }
+  }}
 `
 
 const StudyInfo = props => (
   <Wrapper>
-    <Row noMargin flexEnd noWrap>
-      <Col>
-        <Paragraph>
+    <Row noMargin flexEnd noWrap Tablet>
+      <Col Tablet>
+        <Paragraph Phone>
           Este estudio se basa en la revisión de información pública, principalmente en los sitios de internet de cada empresa, y para esta primera versión, fue realizada en dos etapas:
         </Paragraph>
         <List>
-          <ListItem>
-            <Icon className='fa fa-chevron-right' aria-hidden='true' /> <Span>Enero a Marzo de 2017,</Span> de acuerdo al ranking publicado en junio de 2016.
+          <ListItem Phone>
+            <Icon className='fa fa-chevron-right' aria-hidden='true' /> <Span Phone>Enero a Marzo de 2017,</Span> de acuerdo al ranking publicado en junio de 2016.
           </ListItem>
-          <ListItem>
-            <Icon className='fa fa-chevron-right' aria-hidden='true' /> <Span>Junio de 2017,</Span> en donde se evaluaron las empresas que se integraron al ranking publicado en junio de 2017.
+          <ListItem Phone>
+            <Icon className='fa fa-chevron-right' aria-hidden='true' /> <Span Phone>Junio de 2017,</Span> en donde se evaluaron las empresas que se integraron al ranking publicado en junio de 2017.
           </ListItem>
         </List>
-        <Paragraph>
+        <Paragraph Phone>
           Es posible que las empresas hayan publicado nueva información después de las fechas de revisión descritas, sin embargo, estas actualizaciones no
           modificaron los resultados previamente recabados y por lo tanto no se reflejan en este informe.
         </Paragraph>
       </Col>
       <Col>
-        <ImageContainer>
+        <ImageContainer Tablet>
           <Image src='assets/img/metodologia_img.jpg' />
         </ImageContainer>
       </Col>
